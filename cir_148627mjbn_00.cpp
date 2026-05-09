927a524f6208dfe8ccdc4e4fad5791ed3b606c03
Harshit Choudhary <theharshitchaudhary000@gmail.com>
2026-05-10 01:19:08 +0530

    feat: auto-generated(import): add cir_148627mjbn_00.cpp (from cir.cpp) [2026-05-10 01:19]

---
+++ cir_148627mjbn_00.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Shape{
+
+public:
+
+virtual void draw()=0;
+
+};
+
+class Circle : public Shape{
+
+public:
+
+void draw(){
+cout<<"Circle drawn";
+}
+
+};
+
+int main(){
+
+Circle c;
+c.draw();
+
+
+}
